13805f13c60e769ce78a639e62742f1d953f8c62
Hamna Aamer <hamnaaamer@yahoo.com>
2024-02-18 18:59:13 +0000

    add listener for active tab

--- src/background/background.jsx
+++ src/background/background.jsx
@@ -1,6 +1,7 @@
 function getTabArray() {
   chrome.tabs.query({ currentWindow: true }, (tabs) => {
     const tabArray = tabs.map((tab) => ({
+      window: tab.windowId,
       id: tab.id,
       title: tab.title,
       url: tab.url,
@@ -8,28 +9,39 @@ function getTabArray() {
     }));
 
     chrome.storage.local.set({ returnedTabs: tabArray });
-    for (let index = 0; index < tabArray.length; index++) {
-      console.log("tabs= " + tabArray[index].favIcon + tabArray[index].title);
 
-      if (typeof tabArray[index].favIcon === "string") {
-        //  console.log(tabArray[index].favIcon.length);
-      }
-    }
+    // for (let index = 0; index < tabArray.length; index++) {
+    //   console.log("tabs= " + tabArray[index].favIcon + tabArray[index].title);
+    //   console.log("window id = ", tabArray[index].window);
+    //   if (typeof tabArray[index].favIcon === "string") {
+    //     //  console.log(tabArray[index].favIcon.length);
+    //   }
+    // }
 
-    console.log("savedTabs = " + tabArray);
-    console.log("testing");
+    console.log("savedTabs = ", tabArray);
   });
 }
+
+chrome.windows.getCurrent({ populate: true }, (currentWindow) => {
+  getTabArray();
+})
 
 chrome.tabs.onCreated.addListener(getTabArray);
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && changeInfo.url) {
-    console.log(`Tab: ${tabId} URL changed to ${changeInfo.url}`);
+    // console.log(`Tab: ${tabId} URL changed to ${changeInfo.url}`);
+    getTabArray();
   }
-  getTabArray();
 });
-//maybe create listeners for all events, created, deleted, onupdated.
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  // Check if tabArray has changed
+  if (changes.tabArray) {
+    // Call getTabArray to send updated tabArray to content script
+    getTabArray();
+  }
+});
 
 chrome.tabs.onRemoved.addListener((id) => {
   console.log("removal logged in background", id);
@@ -48,3 +60,22 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("Background script is running after installed");
 });
 console.log("Background script is running"); // okay this works
+
+getTabArray();
+
+chrome.storage.local.get(["returnedTabs"], (result) => {
+  const tabArray = result.returnedTabs || [];
+
+  // Send the data to the content script of all tabs
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      chrome.tabs.sendMessage(tab.id, { tabArray: tabArray });
+    });
+  });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.tabData) {
+    console.log("Received message from button:", message.tabData);
+  }
+});
