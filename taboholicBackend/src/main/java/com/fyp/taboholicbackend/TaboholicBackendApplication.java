4fc33931d5950f3d94a19a4841a59909f7412661
Hamna Aamer <hamnaaamer@yahoo.com>
2024-02-24 19:58:03 +0000

    add spring initialiser backend

---
+++ taboholicBackend/src/main/java/com/fyp/taboholicbackend/TaboholicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.taboholicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaboholicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaboholicBackendApplication.class, args);
+	}
+
+}
---
+++ taboholicBackend/src/test/java/com/fyp/taboholicbackend/TaboholicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.taboholicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaboholicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
